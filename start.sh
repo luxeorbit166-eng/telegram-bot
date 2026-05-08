fb25ff8865909265246e1aba7c46a197e6cc25c8
luxeorbit166-eng <luxeorbit166@gmail.com>
2026-05-08 11:41:12 +0600

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ -f "video_editor_bot_v39_ULTRA.py.gz" ] && [ ! -f "video_editor_bot_v39_ULTRA.py" ]; then
-    gzip -dk video_editor_bot_v39_ULTRA.py.gz
-fi
-python video_editor_bot_v39_ULTRA.py
